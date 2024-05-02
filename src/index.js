589827bed429dabd4e6f09eb0ddc3dbcad8f8937
Nancy <duong.nancy@gmail.com>
2024-05-02 13:59:57 -0700

    Added the search engine functionalities using the API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+
+}
+
+
+function searchCity(city) {
+  let apiKey = "0f09c464cb3936o320tabf9addd38da4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+
 function showSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-
+  searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", showSearchSubmit);
+
+searchCity("San Francisco");
