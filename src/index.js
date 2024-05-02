e09808a4ec5f6b7309955c08284a2a9aff597b69
Nancy <duong.nancy@gmail.com>
2024-05-02 15:13:56 -0700

    Updated weather data

--- src/index.js
+++ src/index.js
@@ -20,12 +20,12 @@ function updateWeather(response) {
 }
 
 function formatDate(date) {
-  let day = date.getDay();
   let minutes = date.getMinutes();
   let hours = date.getHours();
-
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
-
+  
+  let day = days[date.getDay()];
+  
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
