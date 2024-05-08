cb90b908b562c7bbbe45329552dec40ce8d40360
Nancy <duong.nancy@gmail.com>
2024-05-08 16:13:15 -0700

    Added additional API functionalities to forecast content and updated index.html and CSS

--- src/index.js
+++ src/index.js
@@ -51,6 +51,15 @@ function showSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+
+
+
+}
+
 function getForecast(city) {
   let apiKey = "0f09c464cb3936o320tabf9addd38da4";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
@@ -58,29 +67,28 @@ function getForecast(city) {
 
 }
 
-
 function displayForecast(response) {
-  
-  console.log(response.data);
-  let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
   let forecastHtml = "";
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if(index>0 && index<6){
     forecastHtml =
-      forecastHtml +
+    forecastHtml +
     `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">⛅</div>
-            
-            <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temperature-max">
-                <strong>15°</strong>
-              </div>
-              <div class="weather-forecast-temperature-min">9°</div>
-            </div> 
-          </div>`;
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div class="weather-forecast-icon">
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+    </div>
+    <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature-max">
+    <strong>${Math.round(day.temperature.maximum)}°</strong>
+    </div>
+    <div class="weather-forecast-temperature-min">${Math.round(day.temperature.minimum)}°</div>
+    </div> 
+    </div>`;
+  }
   });
+  
+let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
